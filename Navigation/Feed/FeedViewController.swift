69b327c2020a984ce368d8e4b8157e5593f9a2ce
Kirill Komoff <k.komoff@yandex.ru>
2022-01-15 22:18:22 +0300

    HW_3_7 add

---
+++ Navigation/Feed/FeedViewController.swift
@@ -0,0 +1,147 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Artem Novichkov on 12.09.2020.
+//  Copyright © 2020 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+import StorageService
+import SnapKit
+
+
+final class FeedViewController: UIViewController {
+    
+ 
+    var showPost: (()-> Void)?
+    var presentPost: (() -> Void)?
+    
+    private lazy var showNormallyButton: MyCustomButton = {
+        let button =
+            MyCustomButton(
+                title: "Show Post Normally",
+                titleColor: .white,
+                backgroundColor: .systemGray,
+                backgroundImage: nil) {
+                self.showPost?()
+            }
+        button.layer.cornerRadius = 6
+        button.clipsToBounds = true
+        return button
+    }()
+    
+    private lazy var showModallyButton: MyCustomButton = {
+        let button = MyCustomButton(
+            title: "Show Post Modally",
+            titleColor: .white,
+            backgroundColor: .systemGray,
+            backgroundImage: nil) {
+  
+            self.presentPost?()
+        }
+        button.layer.cornerRadius = 6
+        button.clipsToBounds = true
+        return button
+    }()
+    
+    private let checkTextField: UITextField = {
+        let textField = MyCustomTextField(
+            font: UIFont.systemFont(ofSize: 16),
+            textColor: .black,
+            backgroundColor: .white,
+            placeholder: "Enter the word")
+        
+        return textField
+    }()
+    
+    private lazy var checkButton: MyCustomButton = {
+        let button = MyCustomButton(
+            title: "Check the word",
+            titleColor: .white,
+            backgroundColor: .systemGray,
+            backgroundImage: nil) { [weak self] in
+            self?.onCompletion()
+        }
+        
+        button.layer.cornerRadius = 6
+        button.clipsToBounds = true
+        return button
+    }()
+    
+    private let colorLabel: UILabel = {
+        let label = UILabel()
+        label.alpha = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let feedStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.spacing = 35
+        stackView.alignment = .fill
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private let checker: CheckTextField
+    
+    init(checker: CheckTextField) {
+        self.checker = checker
+        super .init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Feed"
+        view.backgroundColor = .cyan
+        checkTextField.delegate = self
+        setupViews()
+    }
+    
+    private func onCompletion() {
+        
+        let enteredWord = checkTextField.text
+        checker.check(word: enteredWord ?? "") { [weak self] result in
+            switch  result {
+            case .correct:
+                self?.colorLabel.backgroundColor = .green
+                self?.colorLabel.alpha = 1
+            case .incorrect:
+                self?.colorLabel.backgroundColor = .red
+                self?.colorLabel.alpha = 1
+            default: self?.colorLabel.alpha = 0
+                
+            }
+        }
+    }
+    
+    private func setupViews() {
+        
+        view.addSubview(feedStackView)
+        
+        feedStackView.addArrangedSubview(showNormallyButton)
+        feedStackView.addArrangedSubview(showModallyButton)
+        feedStackView.addArrangedSubview(checkTextField)
+        feedStackView.addArrangedSubview(checkButton)
+        feedStackView.addArrangedSubview(colorLabel)
+        
+        feedStackView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(50)
+            make.top.equalToSuperview().inset(150)
+        }
+    }
+}
+
+extension FeedViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
---
+++ Navigation/Table/PostTableViewCell.swift
@@ -0,0 +1,114 @@
+//
+//  PostTableViewCell.swift
+//  Navigation
+//
+//  Created by Kirill Komov on 29.07.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+import iOSIntPackage
+
+class PostTableViewCell: UITableViewCell {
+private let processor = ImageProcessor()
+    
+    var post: PostVK? {
+        didSet {
+            authorLabel.text = post?.author
+            postImageView.image = UIImage(named: post?.image ?? "No Image")
+            descriptionLabel.text = post?.description
+            likesLabel.text = "Likes: \(post?.likes ?? 0)"
+            viewsLabel.text = "Views: \(post?.views ?? 0)"
+            
+            if let image =  UIImage(named: post?.image ?? "No Image") {
+                processor.processImage(sourceImage: image, filter: post?.filter ?? .chrome) {
+                    (image) in
+                    postImageView.image = image
+                }
+            }
+        }
+    }
+    
+    private let authorLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.textColor = .black
+        label.textAlignment = .left
+        label.numberOfLines = 2
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let postImageView: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFit
+        image.backgroundColor = .black
+        image.clipsToBounds = true
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .systemGray
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let likesLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let viewsLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubviews(authorLabel, postImageView, descriptionLabel, likesLabel, viewsLabel)
+        
+        let constraints = [
+            authorLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: baseInset),
+            authorLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: baseInset),
+            authorLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -baseInset),
+            
+            postImageView.topAnchor.constraint(equalTo: authorLabel.bottomAnchor, constant: baseInset),
+            postImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            postImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            postImageView.widthAnchor.constraint(equalTo: postImageView.heightAnchor),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: postImageView.bottomAnchor, constant: baseInset),
+            descriptionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: baseInset),
+            descriptionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -baseInset),
+            
+            likesLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: baseInset),
+            likesLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: baseInset),
+            likesLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -baseInset),
+        
+            viewsLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: baseInset),
+            viewsLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -baseInset),
+            viewsLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -baseInset)
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    private var baseInset: CGFloat { return 16 }
+    
+}
